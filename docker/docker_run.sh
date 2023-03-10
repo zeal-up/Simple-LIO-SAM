0a025f8421f142ebb1c0249f45936b719f9241a3
linzy5 <linzy5@xp.com>
2023-03-10 16:12:17 +0800

    [docker] add volume mount into rundocker script

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
 declare script_dir=$(cd $(dirname $0); pwd -P)
+
+declare ARGS=$(getopt -o h,c: --long help,code -n "$0" -- "$@")
+# echo ARGS=[$ARGS]
+eval set -- "${ARGS}"
+# echo formatted parameters=[$@]
+
 declare docker_image_tag="v1.0"
 declare docker_image_name="spl_lio_sam"
 declare docker_image="${docker_image_name}:${docker_image_tag}"
 declare container_name="spl_lio_sam"
+declare code_dir=""
+
+function Help() {
+cat << EOF
+docker_run.sh -- help script to run docker
+
+Usage:
+    docker_run.sh [-h|--help] [-c|--code]
+
+    -h|--help           Show help message
+    -c|--code           Code directory to mount into container
+
+EOF
+}
+
+if [[ $1 ]]; then
+    while true;
+    do
+        case $1 in
+            -h|--help):
+                Help; exit 0; ;;
+            -c|--code):
+                code_dir=$2; shift 2; ;;
+            --)
+                shift; break; ;;
+            *)
+                echo "unrecognized arguments ${@}"
+                Help
+                exit 1
+                ;;
+        esac
+    done
+fi
 
 if [[ $(docker ps -a | grep ${container_name}) ]]; then
     echo "The container '${container_name}' is already runable, start or exec it"
@@ -17,11 +56,16 @@ else
     docker pull ${docker_image}
 fi
 
+declare volumes=""
+if [[ ${code_dir} ]]; then
+    volumes="-v ${code_dir}:/home/splsam/codes"
+fi
+
 docker run -it \
-    -v $HOME:/home/splsam\
     -u $(id -u):$(id -g) \
     --network=host \
     -e HOME=/home/splsam \
+    ${volumes} \
     --name=${container_name} \
     -e "QT_X11_NO_MITSHM=1" \
     -e DISPLAY=unix$DISPLAY \
